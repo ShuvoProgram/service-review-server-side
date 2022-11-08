88f0c70e534f8a82458341f70eeca83c6931b9fc
Shuvo Khan <shuvoprogramer@gmail.com>
2022-11-08 18:24:53 +0600

    added mongodb database

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASSWORD}@cluster0.iqotg9c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('gotravel').collection('services');
+
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            // const count = await serviceCollection.estimatedDocumentCount();
+            res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('Server running....')
